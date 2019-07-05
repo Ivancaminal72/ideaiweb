1d94734e929f7b7e498b3d1c7e824806d7483548
ivancaminal <ivancaminal72@gmail.com>
2019-07-05 22:28:21 +0200

    small fix

--- databases/updater.sh
+++ databases/updater.sh
@@ -3,13 +3,17 @@
 #Exit if an error occurs
 set -e
 
+#Go to the project path
+cd /home/ivan/github/ideaiweb
+
 #Download updated list of projects
 wget -q -O ./csv/competitive.bib https://futur.upc.edu/bibtex/IDEAI-UPC/as/cGFydGljaXBhY2lvcHJvamVjdGVyZGljb21wZXRpdGl1/a/1
 wget -q -O ./csv/non-competitive.bib https://futur.upc.edu/bibtex/IDEAI-UPC/as/cGFydGljaXBhY2lvcHJvamVjdGVyZGlub2NvbXBldGl0aXU=
 
 #Parse and unify with old form inormation
-python ./databases/Projects.py > ~/Desktop/error_projects_ideai.log 2>&1
+/home/ivan/anaconda3/bin/python3.7 ./databases/Projects.py > ~/Desktop/error_projects_ideai.log 2>&1
 if [ ! -s ~/Desktop/error_projects_ideai.log ]; then rm -f ~/Desktop/error_projects_ideai.log; fi
 
 #Upload new data to production server
-./copy_db.sh
+./copy_db.sh > ~/Desktop/error_upload_ideai.log 2>&1
+if [ ! -s ~/Desktop/error_upload_ideai.log ]; then rm -f ~/Desktop/error_upload_ideai.log; fi
